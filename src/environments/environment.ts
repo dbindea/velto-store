e54f207b31ddd71b56802b0e312254559eae1167
Dorel Bindea <dbindea@gmail.com>
2026-06-04 23:46:04 +0200

    firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    appId: 'YOUR_APP_ID'
-  }
+    apiKey: 'AIzaSyCN34whfIFd6EqbOrCAi4HqiR2N4kVrC7s',
+    authDomain: 'velto-store.firebaseapp.com',
+    projectId: 'velto-store',
+    storageBucket: 'velto-store.firebasestorage.app',
+    messagingSenderId: '611339546245',
+    appId: '1:611339546245:web:c15344927adffc3a8c03ec',
+  },
 };
